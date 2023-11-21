e85b6e9309972397ff07ad7e092e0546a4407ed0
BiancaAH <150947009+BiancaAH@users.noreply.github.com>
2023-11-22 00:18:09 +0100

    Update api_script.js

--- js/api_script.js
+++ js/api_script.js
@@ -68,6 +68,76 @@ function searchArticle() {
         });
 }
 
+
+function searchTyres() {
+    const articleNumber = document.getElementById("articleNumberInput").value;
+    const apiUrl = `/article/${articleNumber}`;
+
+    fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/xml',
+        }
+    })
+        .then(response => response.text())
+        .then(data => {
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(data, "text/xml");
+
+            // Extrahiert die Artikel aus dem XML
+            const articles = xmlDoc.querySelectorAll('artikel');
+
+            // Leert vorherige Daten
+            const articleInfoContainer = document.getElementById('articleInfoContainer');
+            articleInfoContainer.innerHTML = '';
+
+            // Regulärer Ausdruck, um die genaue Artikelnummer zu überprüfen
+            const regex = new RegExp(`\\b${articleNumber}\\b`);
+
+            // Überprüfen, ob mehrere Artikel gefunden wurden
+            if (articles.length > 1) {
+                articleInfoContainer.innerHTML = `<p>Mehrere Artikel mit der Nummer ${articleNumber} gefunden. Bitte präzisieren Sie die Suche.</p>`;
+                return;
+            }
+
+            articles.forEach(article => {
+                const herstellernummer = article.getAttribute('herstellernummer');
+                const hersteller = article.getAttribute('hersteller');
+                const beschreibung = article.getAttribute('beschreibung');
+                const nettopreisString = article.querySelector('staffel').getAttribute('nettopreis');
+
+                // Ersetzt Kommas mit Punkten
+                const nettopreis = parseFloat(nettopreisString.replace(',', '.'));
+
+                //Kontrolliert ob die ID eindeutig ist
+                if (regex.test(herstellernummer)) {
+                    // Kalkuliert den VK basierend auf EK
+                    const verkaufspreis = calculateVerkaufspreis(nettopreis);
+
+                    // erstellt einen Container für jeden Artikel
+                    const articleContainer = document.createElement('div');
+                    articleContainer.classList.add('article');
+
+                    // Fülle den Artikelcontainer mit Artikelinformationen
+                    articleContainer.innerHTML = `
+                        <h2>Artikelinformation</h2>
+                        <p><strong>Herstellernummer:</strong> ${herstellernummer}</p>
+                        <p><strong>Hersteller:</strong> ${hersteller}</p>
+                        <p><strong>Beschreibung:</strong> ${beschreibung}</p>
+                        <p><strong>Nettopreis:</strong> ${nettopreis.toFixed(2)} €</p>
+                        <p><strong>Verkaufspreis netto:</strong> ${verkaufspreis.toFixed(2)} €</p>
+                    `;
+
+                    // Füge den Artikelcontainer zum Hauptcontainer hinzu
+                    articleInfoContainer.appendChild(articleContainer);
+                }
+            });
+        })
+        .catch(error => {
+            console.error(error);
+
+        });
+}
 // Funktion zur Berechnung des Verkaufspreises basierend auf dem Nettopreis
 function calculateVerkaufspreis(nettopreis) {
     let verkaufspreis;
